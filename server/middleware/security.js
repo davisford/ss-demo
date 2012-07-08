24b0700901550abec5b42ce58addd6b525f29208
davisford <davisford@gmail.com>
2012-07-08 00:10:42 -0400

    cookie problem

--- server/middleware/security.js
+++ server/middleware/security.js
@@ -1,11 +1,13 @@
-// only let user through if they are admin
+// req.session.userId should be set here if user
+// hit the rpc demo.sendMessage, but it is not
+
 exports.authenticated = function() {
-  return function (req, res, next) {
+  return function(req, res, next) {
     console.log("middleware.authenticated session =>\n", req.session);
-    if(req.session.userId === true) {
+    if (req.session && req.session.userId) {
       return next();
     } else {
       throw new Error('unauthorized');
-    } 
+    }
   };
 };
